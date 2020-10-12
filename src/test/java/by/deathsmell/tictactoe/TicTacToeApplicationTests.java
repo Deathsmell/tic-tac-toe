c8bc8615430f2589c78de4e389468133002e08b0
Alexander Mitskevich <johndeathsmell@gmail.com>
2020-10-12 10:32:13 +0300

    initial

---
+++ src/test/java/by/deathsmell/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package by.deathsmell.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
