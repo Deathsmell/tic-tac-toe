084443cc025c232187f89f655a47d0908b24654f
Alexander Mitskevich <johndeathsmell@gmail.com>
2020-10-14 09:43:20 +0300

    added Navbar

--- src/main/resources/js/app.jsx
+++ src/main/resources/js/app.jsx
@@ -4,17 +4,21 @@ import AuthPage from "./pages/AuthPage";
 import RoomsList from "./pages/RoomsList";
 import {AlertState} from './context/alert/AlertState'
 import {Alert} from "./components/Alert";
+import Navbar from "./components/Navbar";
 
 export default () => {
     return (
         <AlertState>
             <BrowserRouter>
-                <Alert/>
+                <Navbar/>
                 <div className="container pt-4">
-                    <Switch>
-                        <Route path={"/"} component={RoomsList}/>
-                        {/*<Route path={"/"} component={AuthPage}/>*/}
-                    </Switch>
+                    <Alert/>
+                    <div className="container pt-5">
+                        <Switch>
+                            <Route path={"/"} component={RoomsList}/>
+                            {/*<Route path={"/"} component={AuthPage}/>*/}
+                        </Switch>
+                    </div>
                 </div>
             </BrowserRouter>
         </AlertState>
---
+++ src/main/resources/js/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {IoIosLogOut} from 'react-icons/io'
+
+const Navbar = () => {
+
+    return (
+        <nav className="d-flex row navbar navbar-dark bg-primary">
+            <a className="navbar-brand">Tic Tac Toe</a>
+            <a className="nav-link"
+               href="#"
+            >
+                <IoIosLogOut size={"30px"}
+                             color={"white"}
+                />
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar
