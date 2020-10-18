a0607161a57b1e0a370eb26bb3efc3f93cf5e9f7
Alexander Mitskevich <johndeathsmell@gmail.com>
2020-10-18 19:02:28 +0300

    deploy on heroku

--- src/main/java/by/deathsmell/tictactoe/configuration/WebSecurityConfig.java
+++ src/main/java/by/deathsmell/tictactoe/configuration/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 csrf()
                     .disable()
                 .authorizeRequests()
-                    .antMatchers("/login**", "/registration**", "/error**")
+                    .antMatchers("/","/js/**","/login**", "/registration**", "/error**")
                     .permitAll()
                 .anyRequest()
                     .authenticated()
